012e56a77cbe4b30fbd13e74f5692fb708bca837
diegovf <diegov09f@hotmail.es>
2015-04-28 00:34:58 -0600

    añadí las clases

---
+++ src/Modelo/ToOctal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author diego
+ */
+public class ToOctal {
+    
+    /**
+     * 
+     */
+    public ToOctal() {
+    }
+}
