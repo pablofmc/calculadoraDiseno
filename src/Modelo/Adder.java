dc1565e22643ead33e34dbc190ebd9cd9e977202
Pablo Mora <Pablo Mora@PabloLaptop>
2015-04-28 00:43:19 -0600

    DTO

--- src/Modelo/Adder.java
+++ src/Modelo/Adder.java
@@ -7,10 +7,25 @@ import java.util.*;
  */
 public class Adder extends Calculator {
 
+    int resultado;
+    ArrayList<Integer> numeros;
     /**
      * 
      */
     public Adder() {
+       this.resultado = 0;
+    }
+    
+    @Override
+    public int calculate() {
+        for(int i = 0;i <= numeros.size();i++) {
+            resultado += numeros.indexOf(i);
+        }
+        return resultado;
+    }
+    
+    public void getList(ArrayList<Integer> listaNumeros) {
+        this.numeros = listaNumeros;
     }
 
 }
