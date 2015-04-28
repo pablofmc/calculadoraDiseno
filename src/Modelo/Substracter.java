a59fe51cd3eaaeffdfe12403b3f6432d188c4602
Pablo Mora <Pablo Mora@PabloLaptop>
2015-04-28 01:35:58 -0600

    Merge origin/master
    Conflicts:
    	src/Modelo/Adder.java

--- src/Modelo/Substracter.java
+++ src/Modelo/Substracter.java
@@ -7,10 +7,24 @@ import java.util.*;
  */
 public class Substracter extends Calculator {
 
+    int partialResult;
     /**
      * 
      */
     public Substracter() {
+        partialResult = 0;
+    }
+    
+    public String calculate() {
+        for(int i = 0;i <= listNumbers.size();i++) {
+            partialResult -= listNumbers.indexOf(i);
+        }
+        result = Integer.toString(partialResult);
+        return result;
+    }
+    
+    public void getList(ArrayList<Integer> listNumbers) {
+        this.listNumbers = listNumbers;
     }
 
 }
